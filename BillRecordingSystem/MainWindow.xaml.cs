734349ce490dec0e5b74d5a9e4229af6829259e3
Lev Madylius <Lev Madylius@LEV>
2017-05-17 23:51:22 +0300

    Main window My expances tab updated

--- BillRecordingSystem/MainWindow.xaml.cs
+++ BillRecordingSystem/MainWindow.xaml.cs
@@ -18,11 +18,15 @@ namespace BillRecordingSystem
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            
         }
     }
 }
